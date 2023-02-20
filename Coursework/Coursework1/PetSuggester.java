16da3b8b083b711a8c279e21c4470704f22b75f6
Chango <schang09032003@gmail.com>
2023-02-20 13:57:46 +0000

    Feature : Coursework 1

--- Coursework/Coursework1/PetSuggester.java
+++ Coursework/Coursework1/PetSuggester.java
@@ -48,26 +48,31 @@ public class PetSuggester{
   }
 
   public static void main(String[] args) {
+    //Default Tree Creation
     TreeNode defaultTree = new TreeNode("Do you like dogs?", new TreeNode("Do you prefer big dogs?", new TreeNode("a golden retriever"), new TreeNode("a chihuahua")), new TreeNode("an iguana"));
+    //Current Cursor Initiation
     TreeNode currentTree = defaultTree;
-
+    //Check for Game State
     boolean gameState = true;
     System.out.println("Welcome to this ultra fun questionnaire!");
+    //Read from existing files
     try(BufferedReader bf = new BufferedReader(new FileReader("suggestions.txt"))){
       defaultTree = preOrderRead(bf);
       currentTree = defaultTree;
     } catch(IOException e){
+      //If file cannot be found, don't do anything
     }
     do{
+      //Standard Question Flow
       if(askYesNo(currentTree.getValue())){
         currentTree = currentTree.getLeft();
       } else {
         currentTree = currentTree.getRight();
       }
-
       if(currentTree.isLeaf()){ //Check for end of tree
         System.out.println(currentTree.getValue());
         if(!askYesNo("Are you satisfied with your answer?")){ //Add question
+        //Add Question flow
           System.out.println("Enter name of preferred pet: ");
           String newEntry = scanner.nextLine();
           System.out.println("Enter question that is true for pet: ");
@@ -76,12 +81,14 @@ public class PetSuggester{
           currentTree.setRight(new TreeNode(currentTree.getValue()));
           currentTree.setValue(newQuestion);
         }
+        //Restart Questionaire
         if(askYesNo("Would you like to play again?")){
           gameState = true;
           currentTree = defaultTree;
           continue;
         } else {
-        gameState = false;
+        gameState = false; //End Game Flow
+        //Tree Saving Flow
           if(askYesNo("Do you want to save the current tree?")){
             try(BufferedWriter bf = new BufferedWriter(new FileWriter("suggestions.txt"))){
               String savedTree = preOrderWrite(defaultTree);
