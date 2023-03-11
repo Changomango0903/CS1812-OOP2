88f6595e920101bfe0253d8e37e90dfa06f1cd18
Chango <schang09032003@gmail.com>
2023-03-11 20:01:25 +0000

    Replaced DataInputStream with BufferedReader class

--- Coursework/Coursework3/HollomonClient.java
+++ Coursework/Coursework3/HollomonClient.java
@@ -3,6 +3,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.net.Socket;
@@ -25,14 +26,14 @@ public class HollomonClient{
     }
   }
   public List<Card> login(String username, String password){
-    try(InputStream is = socket.getInputStream()){
+    try(BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
       try(OutputStream os = socket.getOutputStream()){
         String fUsername = username + "\n";
         String fPassword = password + "\n";
         os.write(fUsername.getBytes());
         os.write(fPassword.getBytes());
         os.flush();
-        String response = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+        String response = br.readLine();
         System.out.println(response);
         if(response.equalsIgnoreCase("Incorrect password.")){
           return null;
