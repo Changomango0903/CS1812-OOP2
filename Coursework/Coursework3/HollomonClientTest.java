25567f8e9dec5923c2d424fa49af26256255a053
Chango <schang09032003@gmail.com>
2023-03-18 18:44:57 +0000

    3/18/2023 : Deprecated Collections.sort() functionality

--- Coursework/Coursework3/HollomonClientTest.java
+++ Coursework/Coursework3/HollomonClientTest.java
@@ -1,11 +1,14 @@
+import java.util.List;
+
 public class HollomonClientTest{
     public static void main(String[] args) {
         HollomonClient hc0 = new HollomonClient("netsrv.cim.rhul.ac.uk", 1812);
-        System.out.println(hc0.login("bottest", "testpass"));
-        HollomonClient hc1 = new HollomonClient("netsrv.cim.rhul.ac.uk", 1812);
-        System.out.println(hc1.login("iamnotauser", "maybethisisapassword"));
-        hc0.close();
-        hc1.close();
+        List<Card> cardList = hc0.login("bottest", "testpass");
+        
+
+        for(int i = 0; i < cardList.size(); i++){
+            System.out.println(cardList.get(i));
+        }
 
     }
 }
