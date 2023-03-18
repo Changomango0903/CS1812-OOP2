0b67a48e0e790788c0932e2d716c9df140797912
Chango <schang09032003@gmail.com>
2023-03-18 19:58:40 +0000

    3/18/2023 : Implemented getCredits function

--- Coursework/Coursework3/HollomonClientTest.java
+++ Coursework/Coursework3/HollomonClientTest.java
@@ -3,12 +3,17 @@ import java.util.List;
 public class HollomonClientTest{
     public static void main(String[] args) {
         HollomonClient hc0 = new HollomonClient("netsrv.cim.rhul.ac.uk", 1812);
-        List<Card> cardList = hc0.login("bottest", "testpass");
-        Card[] cardTest = {new Card(2, "test1", Rank.COMMON), new Card(1, "test2", Rank.UNIQUE), new Card(3, "atest", Rank.COMMON)};
+        try{
+            List<Card> cardList = hc0.login("bottest", "testpass");
+            Card[] cardTest = {new Card(2, "test1", Rank.COMMON), new Card(1, "test2", Rank.UNIQUE), new Card(3, "atest", Rank.COMMON)};
 
-        for(int i = 0; i < cardList.size(); i++){
-            System.out.println(cardList.get(i));
+            for(int i = 0; i < cardList.size(); i++){
+                System.out.println(cardList.get(i));
+            }
+        } catch(Exception e){
+            e.printStackTrace();
         }
+        
 
     }
 }
