657ee79221ad38f5d6b46cceeb823c681d06d0eb
Chango <schang09032003@gmail.com>
2023-03-18 18:34:26 +0000

    3/18/2023 : Refactored Code

--- Coursework/Coursework3/HollomonClient.java
+++ Coursework/Coursework3/HollomonClient.java
@@ -1,15 +1,11 @@
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.Reader;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 public class HollomonClient{
   String server;
